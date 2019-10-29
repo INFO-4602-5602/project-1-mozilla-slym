24d25ba0e762a022941beeab738e00c7e0f118b0
Lulu Kangaroo <lululiu85@gmail.com>
2019-10-28 21:24:46 -0600

    Updates

---
+++ precache-manifest.83958f10ead94d6339dddddf18a90921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2aebdca0b5c7c167f0",
+    "url": "/INFO-4602-5602/project-1-mozilla-slym/static/css/main.33904e41.chunk.css"
+  },
+  {
+    "revision": "6b2aebdca0b5c7c167f0",
+    "url": "/INFO-4602-5602/project-1-mozilla-slym/static/js/main.d7c74f51.chunk.js"
+  },
+  {
+    "revision": "cede01e19c9d14bb9752",
+    "url": "/INFO-4602-5602/project-1-mozilla-slym/static/js/runtime~main.02c91079.js"
+  },
+  {
+    "revision": "b4c946e0bbe05cf46fc4",
+    "url": "/INFO-4602-5602/project-1-mozilla-slym/static/js/2.fa40e270.chunk.js"
+  },
+  {
+    "revision": "9f68e81ee96664718d8be89213883f83",
+    "url": "/INFO-4602-5602/project-1-mozilla-slym/index.html"
+  }
+];
