f91969e7f06369f017175b820662b15847060672
Lulu Liu <lululiu85@gmail.com>
2019-10-29 10:44:21 -0600

    added simple bar chart example to quantitative

--- slym_viz/src/vizes/quantitative.js
+++ slym_viz/src/vizes/quantitative.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './quantitative.css';
+import BarChart from './barChart'
 
 class Quantitative extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Quantitative graph</h1>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Quantitative graph</h1>
+        <BarChart data={[5, 10, 1, 3]} size={[500, 500]} />
+      </div>
+    )
   }
-  export default Quantitative;
+}
+
+export default Quantitative;
