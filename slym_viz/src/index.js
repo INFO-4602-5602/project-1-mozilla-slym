c6dbd19917dbbfd1d8448f332349bb21cfec982e
Lulu Liu <lululiu85@gmail.com>
2019-11-05 19:24:19 -0700

    Update index.js

---
+++ slym_viz/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom';
+// CSS Styling
+import './index.css';
+// Components
+import Categorical from './vizes/categorical';
+import Quantitative from './vizes/quantitative';
+
+const router = (
+  <Router basename={"/project-1-mozilla-slym"}>
+      <div className="navContainer">
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active" to="/">Categorical</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/quantitative">Quantitative</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Categorical} />
+          <Route path="/quantitative" component={Quantitative} />
+        </Switch>
+      </div>
+  </Router>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
